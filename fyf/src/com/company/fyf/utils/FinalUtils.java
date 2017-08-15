c2d063e64a8460e902967b08c7cceeb5df9337fd
liyaxing <liyaxing@58ganji.com>
2017-08-15 20:19:04 +0800

    导入bugly类库

--- fyf/src/com/company/fyf/utils/FinalUtils.java
+++ fyf/src/com/company/fyf/utils/FinalUtils.java
@@ -8,13 +8,6 @@ import net.tsz.afinal.FinalDb;
 import net.tsz.afinal.FinalDb.DbUpdateListener;
 import net.tsz.afinal.FinalHttp;
 
-import org.apache.http.client.CookieStore;
-import org.apache.http.cookie.Cookie;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 public class FinalUtils {
 	
 	private static FinalBitmap finalBitmap ;
@@ -43,37 +36,7 @@ public class FinalUtils {
 		return db ;
 	}
 	
-	private static CookieStore cookieStore = new CookieStore() {
-		
-		private  List<Cookie>  cookies = new ArrayList<>() ;
 
-		@Override
-		public void addCookie(Cookie cookie) {
-			// TODO Auto-generated method stub
-			Logger.d("FinalUtils", "[cookieStore addCookie] cookie = " + cookie) ;
-			cookies.add(cookie) ;
-		}
-
-		@Override
-		public void clear() {
-			// TODO Auto-generated method stub
-			Logger.d("FinalUtils", "[cookieStore clear]") ;
-		}
-
-		@Override
-		public boolean clearExpired(Date date) {
-			// TODO Auto-generated method stub
-			Logger.d("FinalUtils", "[cookieStore clearExpired] date = " + date) ;
-			return false;
-		}
-
-		@Override
-		public List<Cookie> getCookies() {
-			Logger.d("FinalUtils", "[cookieStore getCookies]") ;
-			return cookies;
-		}
-	};
-	
 	
 
 }
