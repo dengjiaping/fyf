34b6f89babab85f36a9d4402eb5a22b89892cebc
liyaxing <liyaxing@58ganji.com>
2017-08-15 23:43:18 +0800

    预约模块布局

---
+++ fyf/src/com/company/fyf/ui/O01OrderDialog.java
@@ -0,0 +1,15 @@
+package com.company.fyf.ui;
+
+import android.content.Context;
+
+/**
+ * Created by liyaxing on 2017/8/15.
+ */
+public class O01OrderDialog extends B03BaseDialog{
+
+
+
+    public O01OrderDialog(Context context) {
+        super(context);
+    }
+}
